514104834656d5b81f49c37a28fee5ab878ed2d5
Mariem KHAYECH <mariem.khayech@ORTEC-GROUP.DOM>
2017-08-23 16:52:06 +0200

    zef

---
+++ src/app/pages/pointage/pointage.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { LocalDataSource } from 'ng2-smart-table';
+import { ActivitiesUserService } from '../../services/activitiesUser/activitiesUser.service';
+import { ActivitiesService } from '../../services/activities/activities.service';
+import { User } from '../../models/user';
+import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
+import { Activity } from '../../models/activity';
+
+@Component({
+    selector: 'pointage',
+    templateUrl: './pointage.html',
+    styleUrls: ['./pointage.scss'],
+})
+export class PointageComponent implements OnInit {
+  
+    constructor(private service: ActivitiesUserService ){
+  
+    }
+
+    public ngOnInit() {
+   
+    }
+
+   
+    
+}
